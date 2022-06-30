8fa5904472418eb1b540ae3def40942d70c79f2a
hippo <109public@gmail.com>
2022-06-30 23:04:30 +0800

    imgur2

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 
 import pyimgur
+from imgurpython import ImgurClient
 import random
 
 
@@ -8,58 +9,79 @@ import random
 ## heruko
 def morning():
 
-    r = random.randrange(1, 7)
-
-
-    client_id = 'ac66a0f7414e9ef'
+    r = random.randrange(0, 6)
     print(r)
+
+    client = ImgurClient('07f717179f3bd69', 'f3e15f93b6e66c05aff4f91b2b2f7519d2bc1eb3')
+    items = client.get_album_images('NzTojyH')
+
+    '''
+    client_id = '51503dac7c6d0b9'
     path = '早安/' + '長輩圖 ' + '(' + str(r) + ')' + '.jpg'
 
     im = pyimgur.Imgur(client_id)
     upload_image = im.upload_image(path)
+    '''
 
-    return upload_image.link
+    return items[r].link
 
 
 
 def afternoon():
 
-    r = random.randrange(1, 8)
-
-    client_id = '51503dac7c6d0b9'
+    r = random.randrange(0, 7)
     print(r)
+
+    client = ImgurClient('07f717179f3bd69', 'f3e15f93b6e66c05aff4f91b2b2f7519d2bc1eb3')
+    items = client.get_album_images('QuqQLOK')
+
+    '''
+    client_id = '51503dac7c6d0b9'
     path = '午安/' + '長輩圖 ' + '(' + str(r) + ')' + '.jpg'
 
     im = pyimgur.Imgur(client_id)
     upload_image = im.upload_image(path)
+    '''
 
-    return upload_image.link
+    return items[r].link
 
 def goodnight():
 
-    r = random.randrange(1, 7)
-
-    client_id = '07f717179f3bd69'
+    r = random.randrange(0, 7)
     print(r)
+    
+    client = ImgurClient('07f717179f3bd69', 'f3e15f93b6e66c05aff4f91b2b2f7519d2bc1eb3')
+    items = client.get_album_images('xLshDgQ')
+
+    '''
+    client_id = '07f717179f3bd69'
     path = '晚安/' + '長輩圖 ' + '(' + str(r) + ')' + '.jpg'
 
     im = pyimgur.Imgur(client_id)
     upload_image = im.upload_image(path)
+    '''
 
-    return upload_image.link
+    return items[r].link
 
 
 def hell():
 
-    r = random.randrange(1, 8)
-
-    client_id ='aa4cdef37c76c22'
+    r = random.randrange(0, 7)
     print(r)
+
+    client = ImgurClient('07f717179f3bd69', 'f3e15f93b6e66c05aff4f91b2b2f7519d2bc1eb3')
+    items = client.get_album_images('iJ2Vho3')
+
+    '''
+    client_id = '07f717179f3bd69'
     path = '地獄/' + '地獄 ' + '(' + str(r) + ')' + '.jpg'
+
+
     im = pyimgur.Imgur(client_id)
     upload_image = im.upload_image(path)
+    '''
 
-    return upload_image.link
+    return items[r].link
 
 
 app = Flask(__name__)
@@ -102,7 +124,7 @@ def webhook():
     elif json['queryResult']['parameters']['any'] == "晚安":
     
         msg = goodnight()
-        res_message =   {"fulfillmentMessages" : [ {"image" : { "imageUri" : msg } } ] } 
+        res_message =   {"fulfillmentMessages" : [ {"image" : { "imageUri" : 'https://i.imgur.com/mETu5si.jpg' } } ] } 
 
     return make_response(jsonify(res_message))
 
